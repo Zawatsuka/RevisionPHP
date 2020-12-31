c9899558c9f475530582670cb02defbd8162087b
Zawatsuka <thomaslucas2904@gmail.com>
2020-12-31 15:49:31 +0100

    exo3 fini

---
+++ EX3/index.php
@@ -0,0 +1,11 @@
+<?php
+    $tableau = array(
+    "Marc",
+    "Sophie",
+    "Maguy",
+    "Jonathan",
+    "Jennifer");
+    foreach($tableau  as $value){
+        echo "bonjour ". $value ."</br>" ;
+    }
+?>
